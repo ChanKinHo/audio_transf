d078bc5d586d64ca5a0bdd5f43c3d5685269c1a7
ChanKinHo <876204671@qq.com>
2023-02-13 07:07:59 +0800

    配音生成项目创建

---
+++ src/main/java/com/house/audiotransf/AudioTransfApplication.java
@@ -0,0 +1,13 @@
+package com.house.audiotransf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTransfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioTransfApplication.class, args);
+    }
+
+}
